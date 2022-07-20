02b54aa330f0fcb25810b5303cde9655a0582676
Thitichaya Pounglaowech <thitichaya5422@gmail.com>
2022-07-20 19:20:37 +0700

    re_127

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,13 +6,13 @@
 /*   By: tpoungla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 17:46:19 by tpoungla          #+#    #+#             */
-/*   Updated: 2022/07/20 19:16:31 by tpoungla         ###   ########.fr       */
+/*   Updated: 2022/07/20 19:20:05 by tpoungla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isprint(int arg)
 {
-	if (arg <= 126 && arg >= 32)
+	if (arg <= 127 && arg >= 32)
 		return (1);
 	return (0);
 }
