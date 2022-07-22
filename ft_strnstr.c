0009724caadd35f3afd204ae929c1efd2aa1fac4
Thitichaya Pounglaowech <thitichaya5422@gmail.com>
2022-07-22 20:02:28 +0700

    draft

---
+++ ft_strnstr.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpoungla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/22 18:08:53 by tpoungla          #+#    #+#             */
+/*   Updated: 2022/07/22 19:26:20 by tpoungla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+    size_t      i;
+    size_t      n;
+    size_t      j;
+    char    *ptr;
+
+    ptr = NULL;
+    i = 0;
+    if (little == NULL)
+        return ((char*)big);
+    n = ft_strlen(little);
+    while (big[i] && i < len)
+    {
+        j = 0;
+        while (little[j])
+        {
+            //if (little[j] == big [i + j])
+                j++;
+        }
+        i++;
+    }
+    return (ptr);
+}
