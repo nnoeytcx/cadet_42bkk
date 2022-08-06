ee49a853d0b39e5051581b15b94493583d120519
Thitichaya Pounglaowech <thitichaya5422@gmail.com>
2022-08-07 02:05:56 +0700

    edit

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,37 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpoungla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/22 18:08:53 by tpoungla          #+#    #+#             */
-/*   Updated: 2022/07/23 08:48:14 by tpoungla         ###   ########.fr       */
+/*   Created: 2022/08/07 02:01:49 by tpoungla          #+#    #+#             */
+/*   Updated: 2022/08/07 02:01:49 by tpoungla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strdup(const char *s)
 {
-	size_t		i;
-	size_t		n;
-	size_t		j;
+	char	*res;
+	size_t	len;
 
-	i = 0;
-	if (big == NULL || len == 0)
-		return (NULL);
-	if (little == NULL)
-		return ((char *)big);
-	n = ft_strlen(little);
-	while (big[i] && i <= len - n)
+	len = ft_strlen(s) + 1;
+	res = malloc(sizeof(char) * len);
+	if (!res)
+		return (0);
+	while (s)
 	{
-		j = 0;
-		while (big[i + j] && little[j] && little[j] == big [i + j])
-			j++;
-		if (j == n)
-			return ((char *)&big[i]);
-		i++;
+		*res = *s;
 	}
-	return (NULL);
+	return (res);
 }
