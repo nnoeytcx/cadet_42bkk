6b54629d9db61a477fa0c2f2cf737f27f14cacea
Thitichaya Pounglaowech <thitichaya5422@gmail.com>
2022-08-06 23:38:23 +0700

    edit

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -17,23 +17,19 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t		i;
 	size_t		n;
 	size_t		j;
-	char		*ptr;
 
-	ptr = NULL;
 	i = 0;
-	if (len == 0 || big == NULL)
-		return (NULL);
 	if (little == NULL)
 		return ((char *)big);
 	n = ft_strlen(little);
 	while (big[i] && i <= len - n)
 	{
 		j = 0;
-		while (little[j] && little[j] == big [i + j])
+		while (big[i + j] && little[j] && little[j] == big [i + j])
 			j++;
 		if (j == n)
 			return ((char *)&big[i]);
 		i++;
 	}
-	return (ptr);
+	return (NULL);
 }
