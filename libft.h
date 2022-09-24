f441ea4ab12e07013a82cf5e0e4a923ed37c86e4
Thitichaya Pounglaowech <thitichaya5422@gmail.com>
2022-09-24 17:15:04 +0700

    mem

--- libft.h
+++ libft.h
@@ -48,5 +48,9 @@ char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *b, size_t len);
 void	*ft_calloc(size_t count, size_t size);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
