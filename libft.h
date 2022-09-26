805bdfee5e882d12d8085ca04d15f12b5054d3ac
Thitichaya Pounglaowech <thitichaya5422@gmail.com>
2022-09-26 17:44:59 +0700

    bonus4

--- libft.h
+++ libft.h
@@ -61,5 +61,6 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
