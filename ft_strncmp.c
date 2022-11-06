ecd6ceb8469979af76284c9f762e05c9e4bd651d
Thitichaya Pounglaowech <thitichaya5422@gmail.com>
2022-11-07 04:42:50 +0700

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,15 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && i < n)
+	while ((s1[i] || s2[i]) && i < n)
 	{
-		if ((unsigned char)s1[i] < (unsigned char)s2[i])
-			return (-1);
-		if ((unsigned char)s1[i] > (unsigned char)s2[i])
-			return (1);
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	if (i < n && s2[i])
-		return (-1);
 	return (0);
 }
